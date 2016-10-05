dc612510fc7fcd984a45c2f5b7ea7b64521df9bf
Matthias Kuhn <matthias@opengis.ch>
2016-10-05 15:34:56 +0200

    UX improvements

--- qfieldsync/utils/qgis_utils.py
+++ qfieldsync/utils/qgis_utils.py
@@ -28,12 +28,12 @@ def warn_project_is_dirty(text=None):
     if (QgsProject.instance().isDirty()):
         title = tr('Continue?')
         default_text = tr('The currently open project is not saved. '
-                         '\nQFieldSync will overwrite it. \nContinue?')
+                         '\nDo you want to save the project before proceeding?')
         if text is None:
             text = default_text
         answer = QMessageBox.question(None, title, text,
-                                      QMessageBox.Yes | QMessageBox.No | QMessageBox.Save)
-        if answer == QMessageBox.No:
+                                      QMessageBox.Discard | QMessageBox.Cancel | QMessageBox.Save)
+        if answer == QMessageBox.Cancel:
             return False
         if answer == QMessageBox.Save:
             QgsProject.instance().write()
