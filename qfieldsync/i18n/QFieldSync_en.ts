0e2b22f534a839f40a77f2fceecceb0f9d4d9912
Matthias Kuhn <matthias@opengis.ch>
2016-11-21 14:32:58 +0100

    Translation update

--- qfieldsync/i18n/QFieldSync_en.ts
+++ qfieldsync/i18n/QFieldSync_en.ts
@@ -1,21 +1,22 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.0">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
 <context>
     <name>@default</name>
     <message>
         <location filename="test_translations.py" line="48"/>
         <source>Good morning</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qgis_utils.py" line="29"/>
         <source>Continue?</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qgis_utils.py" line="30"/>
         <source>The currently open project is not saved. 
 Do you want to save the project before proceeding?</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -23,27 +24,27 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="basemap.py" line="78"/>
         <source>Map Theme</source>
-        <translation>Kartenthema</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="basemap.py" line="79"/>
         <source>Extent</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="basemap.py" line="80"/>
         <source>Tile size</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="basemap.py" line="81"/>
         <source>Map units per pixel</source>
-        <translation>Karteneinheiten pro Pixel</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="basemap.py" line="84"/>
         <source>Output layer</source>
-        <translation>Ausgabelayer</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -51,17 +52,17 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="config_dialog.py" line="72"/>
         <source>No action</source>
-        <translation>Keine Aktion</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="config_dialog.py" line="73"/>
         <source>Offline copy</source>
-        <translation>Offline kopieren</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="config_dialog.py" line="74"/>
         <source>Remove</source>
-        <translation>Entfernen</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -69,17 +70,17 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="pull_dialog.py" line="52"/>
         <source>Synchronize</source>
-        <translation>Synchronisieren</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="pull_dialog.py" line="72"/>
         <source>Synchronize from QField</source>
-        <translation>Synchronisieren von QField</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="pull_dialog.py" line="76"/>
         <source>The project you imported doesn&apos;t seem to be an offline project</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -87,32 +88,32 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="push_dialog.py" line="75"/>
         <source>Create</source>
-        <translation>Kreieren</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog.py" line="161"/>
         <source>Push to QField</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog.py" line="167"/>
         <source>The project export did&apos;t work</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog.py" line="181"/>
         <source>Your project has been exported sucessfully to {export_folder}, please copy the entire folder to the device</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog.py" line="193"/>
         <source>Layers {} require a real time connection</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog.py" line="201"/>
         <source>Layers {} are not supported by QField</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -120,47 +121,47 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="push_dialog_base.ui" line="14"/>
         <source>Prepare project for QField</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog_base.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Project:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog_base.ui" line="36"/>
         <source>TextLabel</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog_base.ui" line="46"/>
         <source>Export Directory</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog_base.ui" line="55"/>
         <source>...</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog_base.ui" line="62"/>
         <source>Automatically open export folder for manual copying</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog_base.ui" line="78"/>
         <source>Progress</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog_base.ui" line="84"/>
         <source>Total</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="push_dialog_base.ui" line="98"/>
         <source>Layer</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -168,52 +169,52 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="remote_options_base.ui" line="14"/>
         <source>Push project to QField</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="remote_options_base.ui" line="22"/>
         <source>Remote layers detected in the project.</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="remote_options_base.ui" line="29"/>
         <source>Please choose how you want to export them.</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="remote_options_base.ui" line="36"/>
         <source>Offline mode: data dump (works without data connection)</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="remote_options_base.ui" line="46"/>
         <source>Hybrid mode: cache and use connection when available</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="remote_options_base.ui" line="53"/>
         <source>Online mode (requires an active data connection on the device)</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="settings.ui" line="14"/>
         <source>QFieldSync settings</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="settings.ui" line="20"/>
         <source>Default Import Directory</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="settings.ui" line="53"/>
         <source>Browse</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="settings.ui" line="41"/>
         <source>Default Export Directory</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -221,37 +222,37 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="qfield_sync.py" line="234"/>
         <source>&amp;QFieldSync</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qfield_sync.py" line="201"/>
         <source>Settings</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qfield_sync.py" line="208"/>
         <source>Sync to QField</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qfield_sync.py" line="214"/>
         <source>Sync from QField</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qfield_sync.py" line="220"/>
         <source>Project Configuration</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qfield_sync.py" line="289"/>
         <source>not successful, see the {} warnings for details</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qfield_sync.py" line="293"/>
         <source>successful</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -259,32 +260,32 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="synchronize_base.ui" line="14"/>
         <source>Syncronize remote project</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="synchronize_base.ui" line="20"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Select the directory containing the&lt;/p&gt;&lt;p&gt;QField project file:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="synchronize_base.ui" line="35"/>
         <source>Browse</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="synchronize_base.ui" line="47"/>
         <source>Progress</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="synchronize_base.ui" line="53"/>
         <source>Total</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="synchronize_base.ui" line="67"/>
         <source>Layer</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -292,7 +293,7 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="remote_options_dialog.py" line="45"/>
         <source>Push</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -300,7 +301,7 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="settings_dialog.py" line="47"/>
         <source>Save</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -308,42 +309,42 @@ Do you want to save the project before proceeding?</source>
     <message>
         <location filename="config_dialog_base.ui" line="14"/>
         <source>Configure Project for QField synchronisation</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="config_dialog_base.ui" line="30"/>
         <source>Layers</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="config_dialog_base.ui" line="151"/>
         <source>Layer</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="config_dialog_base.ui" line="57"/>
         <source>Action</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="config_dialog_base.ui" line="68"/>
         <source>A base map is fully rendered to a raster image. Attributes from layers on a base map are no longer accessible.</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="config_dialog_base.ui" line="71"/>
         <source>Create base map</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="config_dialog_base.ui" line="83"/>
         <source>Single Layer</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="config_dialog_base.ui" line="131"/>
         <source>Map Theme</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
