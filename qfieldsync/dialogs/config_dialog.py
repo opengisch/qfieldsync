59346ab0fc0f80406165595e167355fed99ec660
Matthias Kuhn <matthias@opengis.ch>
2016-11-28 19:13:43 +0100

    Improve tooltips for buttons

--- qfieldsync/dialogs/config_dialog.py
+++ qfieldsync/dialogs/config_dialog.py
@@ -105,6 +105,7 @@ class ConfigDialog(QDialog, FORM_CLASS):
 
             old_action = layer_source.action
             layer_source.action = cbx.itemData(cbx.currentIndex())
+            print('Apply action {} <- {}'.format(layer_source.layer.name(), layer_source.action))
             if layer_source.action != old_action:
                 self.project.setDirty()
 
