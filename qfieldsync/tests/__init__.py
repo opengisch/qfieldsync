43e57bb3a59791cc918f1f19f42da26790341e47
Matthias Kuhn <matthias@opengis.ch>
2016-12-01 14:57:48 +0100

    Code formatting

--- qfieldsync/tests/__init__.py
+++ qfieldsync/tests/__init__.py
@@ -1,8 +1,9 @@
 # import qgis libs so that ve set the correct sip api version
 import pkgutil
 
-import qgis   # pylint: disable=W0611  # NOQA
+import qgis  # pylint: disable=W0611  # NOQA
 import qfieldsync
+
 # On travis, the qgis2compat module is copied into the sourcetree, if it's present, just laod it
 try:
     import qgis2compat.apicompat  # NOQA
@@ -11,7 +12,9 @@ except ImportError:
 
 from qgis.core import QgsMessageLog
 
+
 def debug_log_message(message, tag, level):
     print('{}({}): {}'.format(tag, level, message))
+
 
 QgsMessageLog.instance().messageReceived.connect(debug_log_message)
