c8603ba528a786a12f4fdc019240bc80e71de2d8
Matthias Kuhn <matthias@opengis.ch>
2018-01-10 22:14:10 +0100

    Remove qgis2compat

--- qfieldsync/__init__.py
+++ qfieldsync/__init__.py
@@ -27,9 +27,6 @@ from __future__ import absolute_import
 import os
 import qgis.utils
 
-qgis2compat_min_version = '0.4.3'
-
-
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
     """Load QFieldSync class from file QFieldSync.
@@ -37,35 +34,6 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-
-    plugin_name = os.path.dirname(__file__).split(os.path.sep)[-1]
-    plugin_name = qgis.utils.pluginMetadata(plugin_name, 'name')
-
-    def qgis2compat_version_check():
-        if not qgis.utils.pluginMetadata('qgis2compat', 'version') >= qgis2compat_min_version:
-            raise RuntimeError('The plugin {plugin_name} requires at least version {qgis2compat_min_version} of the '
-                               '`qgis2compat` plugin. Please update it with the plugin manager.'
-                               .format(plugin_name=plugin_name, qgis2compat_min_version=qgis2compat_min_version))
-
-    try:
-        # qgis.PyQt is available in QGIS >=2.14
-        from qgis.PyQt.QtCore import qVersion  # NOQA
-        # qgis.utils.QGis is available in QGIS < 3
-        if hasattr(qgis.utils, 'QGis'):
-            import qgis2compat.apicompat
-            qgis2compat_version_check()
-            qgis2compat.log('apicompat used in {}'.format(plugin_name))
-    except ImportError:
-        try:
-            # we are in QGIS < 2.14
-            import qgis2compat.apicompat
-            qgis2compat_version_check()
-            qgis2compat.log('PyQt and apicompat used in {}'.format(plugin_name))
-        except ImportError:
-            import traceback
-            traceback.print_exc()
-            raise ImportError('The Plugin {} requires the `qgis2compat` plugin. Please install it with the plugin '
-                              'manager. For more information visit http://opengis.ch/qgis2compat'.format(plugin_name))
 
     from qfieldsync.qfield_sync import QFieldSync
     return QFieldSync(iface)
