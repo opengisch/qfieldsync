e1af6a911b7857b494fbd3a2e567458162433de2
Mario Baranzini <mario@opengis.ch>
2020-02-27 09:18:48 +0100

    Remove a missed print

--- qfieldsync/utils/qfieldcloud_utils.py
+++ qfieldsync/utils/qfieldcloud_utils.py
@@ -6,7 +6,6 @@ def login(base_url, username, password):
     """Return the token or None if the request was not successful"""
 
     url = urljoin(base_url, 'api/v1/auth/login/')
-    print('{} {}'.format(base_url, url))
     data = {
         "username": username,
         "password": password,
