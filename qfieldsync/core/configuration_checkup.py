3354f479ada32321ce8e81769292f3c2e83c10c0
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-06-02 09:01:24 +0200

    license headers

--- qfieldsync/core/configuration_checkup.py
+++ qfieldsync/core/configuration_checkup.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+/***************************************************************************
+ QFieldSync
+                              -------------------
+        begin                : 2020
+        copyright            : (C) 2020 by OPENGIS.ch
+        email                : info@opengis.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ """
 
 from qgis.PyQt.QtCore import QObject
 from qgis.core import QgsProject, Qgis
--- qfieldsync/core/preferences.py
+++ qfieldsync/core/preferences.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+/***************************************************************************
+ QFieldSync
+                              -------------------
+        begin                : 2020
+        copyright            : (C) 2020 by OPENGIS.ch
+        email                : info@opengis.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ """
+
+
 import os
 from qfieldsync.setting_manager import SettingManager, Scope, String
 
