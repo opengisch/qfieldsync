3b85baae13f75588523cc9d90179cd0adf7c0413
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-07-13 14:43:11 +0300

    Add basic upload capability of QFieldCloud projects

--- qfieldsync/gui/project_configuration_dialog.py
+++ qfieldsync/gui/project_configuration_dialog.py
@@ -19,19 +19,22 @@
 """
 import os
 
-from qgis.PyQt.QtCore import Qt
+from requests import HTTPError
+
+from qgis.PyQt.QtCore import Qt, QTemporaryDir
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QDialog, QTableWidgetItem, QToolButton, QComboBox, QCheckBox, QMenu, QAction, QWidget, QHBoxLayout
 from qgis.PyQt.uic import loadUiType
 
-from qgis.core import QgsProject, QgsMapLayerProxyModel, Qgis
+from qgis.core import QgsProject, QgsMapLayerProxyModel, Qgis, QgsOfflineEditing
 
-from qfieldsync.core import ProjectConfiguration
+from qfieldsync.core import ProjectConfiguration, OfflineConverter
 from qfieldsync.core.layer import LayerSource, SyncAction
 from qfieldsync.core.project import ProjectProperties
 from qfieldsync.gui.photo_naming_widget import PhotoNamingTableWidget
 from qfieldsync.gui.utils import set_available_actions
-
+from qfieldsync.core.cloud_api import login, logout, create_project, ProjectUploader, get_error_reason
+from qfieldsync.utils.cloud_utils import to_cloud_title
 
 DialogUi, _ = loadUiType(
     os.path.join(os.path.dirname(__file__), '../ui/project_configuration_dialog.ui'),
@@ -73,6 +76,10 @@ class ProjectConfigurationDialog(QDialog, DialogUi):
         self.multipleToggleButton.setAutoRaise(True)
         self.multipleToggleButton.setPopupMode(QToolButton.InstantPopup)
         self.toggle_menu.triggered.connect(self.toggle_menu_triggered)
+
+        self.loginButton.clicked.connect(self.onLoginButtonClicked)
+        self.logoutButton.clicked.connect(self.onLogoutButtonClicked)
+        self.cloudSyncButton.clicked.connect(self.onCloudSyncButtonClicked)
 
         self.singleLayerRadioButton.toggled.connect(self.baseMapTypeChanged)
         self.unsupportedLayersList = list()
@@ -253,3 +260,103 @@ class ProjectConfigurationDialog(QDialog, DialogUi):
                         layer_source.apply()
 
         self.reloadProject()
+
+
+    def onLoginButtonClicked(self):
+        self.loginButton.setEnabled(False)
+        self.logoutButton.setEnabled(False)
+        self.cloudSyncButton.setEnabled(False)
+        self.loginFeedbackLabel.setVisible(False)
+
+        username = self.usernameLineEdit.text()
+        password = self.passwordLineEdit.text()
+
+        try:
+            resp = login(username, password)
+            
+            if self.rememberMeCheckBox.isChecked():
+                # TODO permanently store the token
+                # resp['token']
+                pass
+
+            self.usernameLineEdit.setEnabled(False)
+            self.passwordLineEdit.setEnabled(False)
+            self.loginButton.setEnabled(False)
+            self.logoutButton.setEnabled(True)
+            self.cloudSyncButton.setEnabled(True)
+        except HTTPError as error:
+            self.loginFeedbackLabel.setText("Login failed: {}".format(get_error_reason(error.response)))
+            self.loginFeedbackLabel.setVisible(True)
+            self.loginButton.setEnabled(True)
+
+
+    def onLogoutButtonClicked(self):
+        self.logoutButton.setEnabled(False)
+        self.loginFeedbackLabel.setVisible(False)
+        self.cloudSyncButton.setEnabled(False)
+
+        try:
+            logout()
+            
+            self.usernameLineEdit.setEnabled(True)
+            self.passwordLineEdit.setText("")
+            self.passwordLineEdit.setEnabled(True)
+            self.loginButton.setEnabled(True)
+        except HTTPError as error:
+            self.loginFeedbackLabel.setText("Login failed: {}".format(get_error_reason(error.response)))
+            self.loginFeedbackLabel.setVisible(True)
+            self.logoutButton.setEnabled(True)
+
+
+    def onCloudSyncButtonClicked(self):
+        self.cloudSyncButton.setEnabled(False)
+        self.syncFeedbackLabel.setText('')
+        self.syncFeedbackLabel.setVisible(True)
+
+        (project_id, _) = self.project.readEntry("qfieldcloud", "projectId")
+        is_new_project = False
+
+        if not project_id:
+            is_new_project = True
+
+            self.syncFeedbackLabel.setText(self.tr("Creating cloud project..."))
+
+            try:
+                title = self.project.title() 
+                title = title if title else self.project.fileName()
+                title = title if title else self.tr('Untitled Project')
+                resp = create_project(to_cloud_title(title), self.usernameLineEdit.text(), title)
+                project_id = resp['id']
+
+                self.project.writeEntry("qfieldcloud", "projectId", project_id)
+                self.project.setDirty(True)
+            except HTTPError as error:
+                self.syncFeedbackLabel.setText(self.tr("Failed to create project: {}".format(get_error_reason(error.response))))
+                return
+
+        export_dir = QTemporaryDir().path()
+
+        offline_convertor = OfflineConverter(self.project, export_dir, self.iface.mapCanvas().extent(),
+                                             QgsOfflineEditing())
+        offline_convertor.total_progress_updated.connect(self.on_offline_convertor_total_progress_updated)
+        offline_convertor.convert()
+
+        project_uploader = ProjectUploader(project_id, export_dir, is_new_project)
+        project_uploader.progress_uploaded.connect(self.on_project_progress_uploaded)
+
+        try:
+            project_uploader.upload()
+        except HTTPError as error:
+            self.syncFeedbackLabel.setText(self.tr("Failed to upload project: {}".format(get_error_reason(error.response))))
+            return
+
+        self.cloudSyncButton.setEnabled(True)
+        self.syncFeedbackLabel.setVisible(False)
+
+
+    def on_offline_convertor_total_progress_updated(self, progress, total, msg):
+        self.syncFeedbackLabel.setText(self.tr("{}% {} ".format(int(progress/total), msg)))
+
+
+    def on_project_progress_uploaded(self, progress, total, filename):
+        self.syncFeedbackLabel.setText(self.tr("{}% Uploading {}...".format(int(progress / total), filename)))
