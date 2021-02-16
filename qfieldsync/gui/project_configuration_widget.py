1813705609f40de6b434e7272d1768ad4c62bcee
Ivan Ivanov <ivan.ivanov@suricactus.com>
2021-02-16 16:08:17 +0200

    Don't save or mark the project dirty when the action preference radio has changed

--- qfieldsync/gui/project_configuration_widget.py
+++ qfieldsync/gui/project_configuration_widget.py
@@ -139,6 +139,7 @@ class ProjectConfigurationWidget(WidgetUi, QgsOptionsPageWidget):
 
 
     def onLayerActionPreferenceChanged(self):
+        '''Triggered when prefer online or offline radio buttons have been changed'''
         prefer_online = self.preferOnlineLayersRadioButton.isChecked()
 
         for i in range(self.cloudLayersConfigWidget.layersTable.rowCount()):
@@ -150,15 +151,9 @@ class ProjectConfigurationWidget(WidgetUi, QgsOptionsPageWidget):
             if cmb.itemData(cmb.currentIndex()) == SyncAction.REMOVE:
                 continue
 
-            old_cloud_action = layer_source.cloud_action
-            idx, cloud_action = layer_source.prefered_cloud_action(prefer_online)
-
-            if cloud_action is not None and old_cloud_action != cloud_action:
-                cmb.setCurrentIndex(idx)
-                layer_source.cloud_action = cmb.itemData(cmb.currentIndex()) 
-                layer_source.apply()
-                self.project.setDirty(True)
-
+            idx, _cloud_action = layer_source.prefered_cloud_action(prefer_online)
+            cmb.setCurrentIndex(idx)
+            layer_source.cloud_action = cmb.itemData(cmb.currentIndex())
 
     def baseMapTypeChanged(self):
         if self.singleLayerRadioButton.isChecked():
