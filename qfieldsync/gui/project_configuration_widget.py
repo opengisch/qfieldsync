ec6a454c9bf3bff0041099af9b9e8feb48c448f6
nirvn <nirvn.asia@gmail.com>
2021-06-09 16:20:37 +0700

    Add UI to pick a digitizing logs layer

--- qfieldsync/gui/project_configuration_widget.py
+++ qfieldsync/gui/project_configuration_widget.py
@@ -84,7 +84,7 @@ class ProjectConfigurationWidget(WidgetUi, QgsOptionsPageWidget):
             )
 
         self.advancedSettingsGroupBox.layout().addWidget(
-            self.areaOfInterestExtentWidget, 0, 1
+            self.areaOfInterestExtentWidget, 1, 1
         )
 
         self.preferOnlineLayersRadioButton.clicked.connect(
@@ -132,6 +132,8 @@ class ProjectConfigurationWidget(WidgetUi, QgsOptionsPageWidget):
             self.mapThemeComboBox.addItem(theme)
 
         self.layerComboBox.setFilters(QgsMapLayerProxyModel.RasterLayer)
+        self.digitizingLogsLayerComboBox.setFilters(QgsMapLayerProxyModel.PointLayer)
+        self.digitizingLogsLayerComboBox.setAllowEmptyLayer(True)
 
         self.__project_configuration = ProjectConfiguration(self.project)
         self.createBaseMapGroupBox.setChecked(
@@ -149,10 +151,17 @@ class ProjectConfigurationWidget(WidgetUi, QgsOptionsPageWidget):
         self.mapThemeComboBox.setCurrentIndex(
             self.mapThemeComboBox.findText(self.__project_configuration.base_map_theme)
         )
+
         layer = QgsProject.instance().mapLayer(
             self.__project_configuration.base_map_layer
         )
         self.layerComboBox.setLayer(layer)
+
+        digitizingLogsLayer = QgsProject.instance().mapLayer(
+            self.__project_configuration.digitizing_logs_layer
+        )
+        self.digitizingLogsLayerComboBox.setLayer(digitizingLogsLayer)
+
         self.mapUnitsPerPixel.setValue(self.__project_configuration.base_map_mupp)
         self.tileSize.setValue(self.__project_configuration.base_map_tile_size)
         self.onlyOfflineCopyFeaturesInAoi.setChecked(
@@ -193,6 +202,14 @@ class ProjectConfigurationWidget(WidgetUi, QgsOptionsPageWidget):
         try:
             self.__project_configuration.base_map_layer = (
                 self.layerComboBox.currentLayer().id()
+            )
+        except AttributeError:
+            pass
+        try:
+            self.__project_configuration.digitizing_logs_layer = (
+                self.digitizingLogsLayerComboBox.currentLayer().id()
+                if self.digitizingLogsLayerComboBox.currentLayer()
+                else ""
             )
         except AttributeError:
             pass
