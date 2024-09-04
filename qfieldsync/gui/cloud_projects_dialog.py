9f840a89b04196cbd962e5564cf9a73515aac279
Johnny Sequeira <seqlazjhonny@gmail.com>
2024-09-04 00:19:04 -0600

    Fix hard-to-read dark blue text on dark background scenario in the cloud project list

--- qfieldsync/gui/cloud_projects_dialog.py
+++ qfieldsync/gui/cloud_projects_dialog.py
@@ -40,6 +40,7 @@ from qgis.PyQt.QtGui import (
     QFont,
     QIcon,
     QPainter,
+    QPalette,
     QPen,
     QPixmap,
     QRegularExpressionValidator,
@@ -309,7 +310,9 @@ class CloudProjectsDialog(QDialog, CloudProjectsDialogUi):
 
     def on_projects_cached_projects_started(self) -> None:
         self.projectsStack.setEnabled(False)
-        self.set_feedback(self.tr("Loading projects list…"), Qt.blue)
+        self.set_feedback(
+            self.tr("Loading projects list…"), self.palette().color(QPalette.WindowText)
+        )
 
     def on_projects_cached_projects_error(self, error: str) -> None:
         self.projectsStack.setEnabled(True)
@@ -560,7 +563,7 @@ class CloudProjectsDialog(QDialog, CloudProjectsDialogUi):
                 self.tr(
                     "You don't have any projects, create your first one by clicking the button in the bottom bar."
                 ),
-                Qt.blue,
+                self.palette().color(QPalette.WindowText),
             )
             return
 
