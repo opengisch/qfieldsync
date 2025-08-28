1d04f9cc88d393dc461e62bbb6aab3b465571593
Ivan Ivanov <ivan.ivanov@suricactus.com>
2025-08-28 21:04:22 +0300

    refactor: remove empty imports, ruff F401

--- qfieldsync/tests/__init__.py
+++ qfieldsync/tests/__init__.py
@@ -19,8 +19,8 @@
 
 # TODO @suricactus: check if this file should have any contents at all???
 # import qgis libs so that ve set the correct sip api version
-import pkgutil  # pylint: disable=W0611
+import pkgutil  # noqa: F401
 
-import qgis  # pylint: disable=W0611
+import qgis  # noqa: F401
 
-import qfieldsync  # pylint: disable=W0611
+import qfieldsync  # noqa: F401
