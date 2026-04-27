b76622d30d631fe277fa188eb94be38339223ce7
Ivan Ivanov <ivan.ivanov@suricactus.com>
2026-04-27 17:26:29 +0300

    Remove completely redundant scripts directory as nobody uses the scripts in there

--- scripts/prepare-commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ROOT_DIR=$(git rev-parse --show-toplevel)
-
-autopep8 -r --in-place --ignore=E261,E265,E402,E501 $ROOT_DIR/qfieldsync/core
-autopep8 -r --in-place --ignore=E261,E265,E402,E501 $ROOT_DIR/qfieldsync/gui
